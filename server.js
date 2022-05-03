62b674260c90282dca86d51d909447c71d648288
saianirudh123 <saianirudh196@gmail.com>
2022-05-04 00:56:50 +0530

    commit on master

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = process.env.PORT || 3001
 app.use(express.json())
@@ -9,6 +10,12 @@ app.use('/api/cars/', require('./routes/carsRoute'))
 app.use('/api/users/', require('./routes/userRoute'))
 app.use('/api/bookings', require('./routes/bookingRoute'))
 
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Node js server started at  ${port}`));
