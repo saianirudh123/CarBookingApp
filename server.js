9ad43b4b88b200c5c181d45c6b22009c2ba8079d
saianirudh123 <saianirudh196@gmail.com>
2022-05-04 00:28:22 +0530

    commit on master

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use('/api/bookings', require('./routes/bookingRoute'))
 
 const path = require('path');
 if(process.env.NODE_ENV === 'production'){
-    app.use('/',express.static(client))
+    app.use('/',express.static('client'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'/client/index.html'));
+        res.sendFile(path.resolve(__dirname,'/client/public/index.html'));
     })
 }
 
